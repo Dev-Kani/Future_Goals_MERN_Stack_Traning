3c97055395dc3ac4bea7fec55b5a56e99d97871f
Kanishka Samarathunga <kanishkakcs@gmail.com>
2022-03-05 19:58:18 +0530

    First Auth

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { getGoals, setGoals, updateGoals, deleteGoals } = require('../controllers/goalcontroller')
+const { getGoals, setGoals, updateGoals, deleteGoals } = require('../controllers/goalController')
 
 
 router.route('/').get(getGoals).post(setGoals)
