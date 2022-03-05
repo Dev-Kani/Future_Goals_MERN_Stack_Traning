ba8fc1e1bf13e69d581c7d26131a74821a553231
Kanishka Samarathunga <kanishkakcs@gmail.com>
2022-03-05 12:22:28 +0530

    Express controller added

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+const { getGoals, setGoals, updateGoals, deleteGoals } = require('../controllers/goalcontroller')
+
+
+router.route('/').get(getGoals).post(setGoals)
+router.route('/:id').delete(deleteGoals).put(updateGoals)
+
+// router.get('/', getGoals)
+
+// router.post('/', setGoals)
+
+// router.put('/:id', updateGoals)
+
+// router.delete('/:id', deleteGoals)
+
+
+module.exports = router;
